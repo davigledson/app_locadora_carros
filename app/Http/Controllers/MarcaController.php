afd64071a881cd1baa35b43a1d77343abd9f9724
Davi Gledson <davigledson@outlook.com>
2023-11-03 01:27:34 -0300

    Validações parte 1 - Controle de fluxos

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -53,6 +53,9 @@ class MarcaController extends Controller
     public function show($id)
     {
         $marca = $this->marca->find($id);
+        if($marca === null){
+            return ['erro'=>'Recurso pesquisado não existe'];
+        }
        return $marca;
    }
 
@@ -77,6 +80,9 @@ class MarcaController extends Controller
 
        //$marca->update($request->all());
        $marca = $this->marca->find($id);
+       if($marca === null){
+        return ['erro'=>'Impossivel realizar a atualização. O recurso solicitado não existe'];
+       }
        $marca->update($request->all());
 
        return $marca;
@@ -89,6 +95,9 @@ class MarcaController extends Controller
     {
         //
         $marca = $this->marca->find($id);
+        if($marca === null){
+            return ['erro'=>'Impossível realizar a exclusão. O recurso solicitado não existe'];
+           }
         $marca->delete();
         return ['msg'=>'A marca foi removida como sucesso!'];
     }
