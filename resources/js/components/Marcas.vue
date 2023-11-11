531877cb493a680724e85ec89a5a1af4561a15f6
Davi Gledson <davigledson@outlook.com>
2023-11-11 13:16:45 -0300

    Adicionando os inputs no modal de registros de marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -47,7 +47,32 @@
      <!-- Button trigger modal -->
 
 
-     <modal-component id="modalMarca" titulo="Adicionar Marca"></modal-component>
+     <modal-component id="modalMarca" titulo="Adicionar Marca">
+        <template v-slot:conteudo>
+            <div class="form-group">
+            <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp" texto-ajuda="informe o nome da marca" >
+
+                 <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp" placeholder="adicionar o nome da marca">
+                
+            </input-container-component>
+        </div>
+
+        <div class="form-group">
+            <input-container-component titulo="Imagem" id="novoImagem" id-help="novoImagemHelp" texto-ajuda="Selecione uma imagem no formato png" >
+            
+                 <input type="file" class="form-control" id="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem">
+                
+            </input-container-component>
+        </div>
+        </template>
+
+        <template v-slot:rodape>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">Salvar</button>
+        </template>
+        
+        
+     </modal-component>
 
  </div> 
  </template>
--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -9,11 +9,13 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <slot name="conteudo"></slot>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-        <button type="button" class="btn btn-primary">Salvar</button>
+      <slot name="rodape">
+
+      </slot>
+        
       </div>
     </div>
   </div>
